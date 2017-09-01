d834217c262189bdb1e0f977b6b49975613c03ba
Pavel <dym91@list.ru>
2017-09-01 15:02:34 +0300

    simple get

---
+++ src/main/java/tsc/remindme/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package tsc.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("tsc.remindme.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
