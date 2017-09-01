df7cde602bf6a570c8849adf31d75ea7de1701d1
Pavel <dym91@list.ru>
2017-09-01 16:43:46 +0300

    db configuration

---
+++ src/main/java/tsc/remindme/server/config/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package tsc.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.EnvironmentCapable;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableJpaRepositories("tsc.remindme.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("tsc.remindme.server")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment env;
+
+}
